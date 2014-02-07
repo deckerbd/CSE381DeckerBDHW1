d4c7b64f02bf1a90bb7f93b1461d8d59521c1d49
deckerbd <deckerbd@miamioh.edu>
2014-02-06 20:39:21 -0500

    Initial main commit.
    Added all the files from niihkha and tested to make sure my compiler is
    working. Seems like it is. :)

---
+++ deckerbd_hw1.java
@@ -0,0 +1,11 @@
+
+public class deckerbd_hw1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0; (i<50); i++){
+			System.out.println("Hello World No."+i+"!\nThis is a test.");
+		}
+	}
+
+}
